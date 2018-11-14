b79d8708d3bf2997149a0ca03833ed211c47f205
zhenhua.zhu <1020090284@qq.com>
2018-11-14 17:42:36 +0800

    gradle中targetSdkVersion更改，插件版本信息更改

--- src/org/zywx/wbpalmstar/plugin/tencentlvb/EUExTencentLVB.java
+++ src/org/zywx/wbpalmstar/plugin/tencentlvb/EUExTencentLVB.java
@@ -1,13 +1,19 @@
 package org.zywx.wbpalmstar.plugin.tencentlvb;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.zywx.wbpalmstar.base.BUtility;
 import org.zywx.wbpalmstar.engine.DataHelper;
+import org.zywx.wbpalmstar.engine.EBrowserActivity;
 import org.zywx.wbpalmstar.engine.EBrowserView;
 import org.zywx.wbpalmstar.engine.universalex.EUExBase;
 import org.zywx.wbpalmstar.plugin.tencentlvb.util.MainActivity;
@@ -23,32 +29,60 @@ public class EUExTencentLVB extends EUExBase {
 	public static final String TEXT_IMAGE = "image";
     public static final String BOOLEAN_IS_SHOW_LOG = "isShowLog";
 
+    private String[] vodPlayParams;
+    private String[] publishParams;
+    private String[] livePlayParams;
+
     public EUExTencentLVB(Context context, EBrowserView eBrowserView) {
 		super(context, eBrowserView);
 	}
 
 
     public void vodPlay(String [] params) {
-        if (params.length != 1) {
-            return;
+        vodPlayParams = params;
+        // android6.0以上动态权限申请
+        if (mContext.checkCallingOrSelfPermission(Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED){
+            requsetPerssions(Manifest.permission.RECORD_AUDIO, "请先申请权限"
+                    + Manifest.permission.RECORD_AUDIO, 1);
+        } else {
+            if (params.length != 1) {
+                return;
+            }
+            DataVO dataVO = DataHelper.gson.fromJson(params[0], DataVO.class);
+            actionHandler(dataVO, ACTION_PLAY_VOD);
         }
-        DataVO dataVO = DataHelper.gson.fromJson(params[0], DataVO.class);
-        actionHandler(dataVO, ACTION_PLAY_VOD);
     }
     public void livePlay(String [] params) {
-        if (params.length != 1) {
-            return;
+        livePlayParams = params;
+        // android6.0以上动态权限申请
+        if (mContext.checkCallingOrSelfPermission(Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED){
+            requsetPerssions(Manifest.permission.RECORD_AUDIO, "请先申请权限"
+                    + Manifest.permission.RECORD_AUDIO, 2);
+        } else {
+            if (params.length != 1) {
+                return;
+            }
+            DataVO dataVO = DataHelper.gson.fromJson(params[0], DataVO.class);
+            actionHandler(dataVO, ACTION_PLAY_LIVE);
         }
-        DataVO dataVO = DataHelper.gson.fromJson(params[0], DataVO.class);
-        actionHandler(dataVO, ACTION_PLAY_LIVE);
 
     }
     public void publish(String [] params) {
-        if (params.length != 1) {
-            return;
+        publishParams = params;
+        // android6.0以上动态权限申请
+        if (mContext.checkCallingOrSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            requsetPerssions(Manifest.permission.CAMERA, "请先申请权限"
+                    + Manifest.permission.CAMERA, 3);
+        } else {
+            if (params.length != 1) {
+                return;
+            }
+            DataVO dataVO = DataHelper.gson.fromJson(params[0], DataVO.class);
+            actionHandler(dataVO, ACTION_PUBLISH);
         }
-        DataVO dataVO = DataHelper.gson.fromJson(params[0], DataVO.class);
-        actionHandler(dataVO,ACTION_PUBLISH);
     }
     public void actionHandler(String [] params, int action) {
         try {
@@ -95,4 +129,44 @@ public class EUExTencentLVB extends EUExBase {
 		return true;
 	}
 
+    @Override
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults[0] != PackageManager.PERMISSION_DENIED) {
+                vodPlay(vodPlayParams);
+            } else {
+                // 对于 ActivityCompat.shouldShowRequestPermissionRationale
+                // 1：用户拒绝了该权限，没有勾选"不再提醒"，此方法将返回true。
+                // 2：用户拒绝了该权限，有勾选"不再提醒"，此方法将返回 false。
+                // 3：如果用户同意了权限，此方法返回false
+                // 拒绝了权限且勾选了"不再提醒"
+                if (!ActivityCompat.shouldShowRequestPermissionRationale((EBrowserActivity) mContext, permissions[0])) {
+                    Toast.makeText(mContext, "请先设置权限" + permissions[0], Toast.LENGTH_LONG).show();
+                } else {
+                    requsetPerssions(Manifest.permission.RECORD_AUDIO, "请先申请权限" + permissions[0], 1);
+                }
+            }
+        } else if (requestCode == 2){
+            if (grantResults[0] != PackageManager.PERMISSION_DENIED){
+                livePlay(livePlayParams);
+            } else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale((EBrowserActivity)mContext, permissions[0])) {
+                    Toast.makeText(mContext, "请先设置权限" + permissions[0], Toast.LENGTH_LONG).show();
+                } else {
+                    requsetPerssions(Manifest.permission.RECORD_AUDIO, "请先申请权限" + permissions[0], 1);
+                }
+            }
+        }else if (requestCode == 3){
+            if (grantResults[0] != PackageManager.PERMISSION_DENIED){
+                publish(publishParams);
+            } else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale((EBrowserActivity)mContext, permissions[0])) {
+                    Toast.makeText(mContext, "请先设置权限" + permissions[0], Toast.LENGTH_LONG).show();
+                } else {
+                    requsetPerssions(Manifest.permission.CAMERA, "请先申请权限" + permissions[0], 1);
+                }
+            }
+        }
+    }
 }
